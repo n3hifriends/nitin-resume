edf8ce72eb5a20b743c9734c13cf21e1c5c8cfe1
Nitin Kalokhe <nitin3kalokhe@gmail.com>
2025-08-21 19:19:17 +0530

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class", // Enable dark mode using the 'class' strategy
   theme: {
     extend: {
-      colors: {
-        background: 'hsl(0 0% 100%)',
-        foreground: 'hsl(222.2 47.4% 11.2%)',
-        card: 'hsl(0 0% 100%)',
-        'muted-foreground': 'hsl(215 16.3% 46.9%)',
-        accent: 'hsl(210 40% 96.1%)',
-        primary: 'hsl(221.2 83.2% 53.3%)',
-        secondary: 'hsl(199 89% 48%)',
-      }
+      fontFamily: {
+        muli: ["Muli", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
